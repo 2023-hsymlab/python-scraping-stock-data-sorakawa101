dfa79ecb75aeb6ecb0456f8b210ffbd1c9de6c66
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-06-07 02:47:28 +0900

    add: mysqlと接続テスト完了

--- VirtualStockTradeSystem/home.py
+++ VirtualStockTradeSystem/home.py
@@ -10,3 +10,14 @@ Run Command is:
 streamlit run home.py
 ```
 """
+
+
+# Initialize connection.
+conn = st.experimental_connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from mytable;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
