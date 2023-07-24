132af5fefd39f4a8edd05bf9a2fcb7173f146921
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-07-24 15:38:38 +0900

    add: ページ設定を追加 remove: .envファイルを削除してsecrets.tomlにまとめた

--- VirtualStockTradeSystem/home.py
+++ VirtualStockTradeSystem/home.py
@@ -1,8 +1,14 @@
 import streamlit as st
 
+# ページ設定
+st.set_page_config(
+    page_title="Virtual Stock Trade System",
+    page_icon="🧊",
+    layout="wide",
+)
 
-st.title('Virtual Stock Trade System')
-st.header('Home')
+st.title("Virtual Stock Trade System")
+st.header("Home")
 
 """
 Run Command is:
